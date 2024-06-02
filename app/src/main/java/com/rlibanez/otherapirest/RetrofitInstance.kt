88fce8fa0a698787e2c9b3a30027f77afbf77ce9
rlibanez <rlibanezz@gmail.com>
2024-06-02 16:01:29 +0200

    Add Retrofit Instance

---
+++ app/src/main/java/com/rlibanez/otherapirest/RetrofitInstance.kt
@@ -0,0 +1,16 @@
+package com.rlibanez.otherapirest
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "http://192.168.2.2:8008/api/"
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
